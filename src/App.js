d19486f8541b6b5b6bf3bb7222addf1487dcb63c
Rabit <rabit2245@gmail.com>
2021-11-17 22:23:30 +0100

    Implemented useEffect hook to increment the count

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    const newCountValue = count + 1;
-    setCount(newCountValue);
-  };
+  useEffect(() => {
+  // console.log(count);
+  }, [count]);
+
   return (
     <div className='border-2 m-4 p-4 border-blue-300'>
       <h2 className='p-b2 mb-2 font-semibold text-lg '>{count}</h2>
       <button
         className='border-2 text-xl font-semibold border-blue-300 px-4 py-1 rounded-lg'
-        onClick={handleClick}
+        onClick={() => setCount(count + 1)}
       >
         Increment
       </button>
